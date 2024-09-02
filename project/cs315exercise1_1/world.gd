189ffde65ded6550a731a7bb487ccbcdc28eb790
[William Robinson] <william.robinson@bsu.edu>
2024-09-02 16:31:57 -0400

    Remove exit button

--- project/cs315exercise1_1/world.gd
+++ project/cs315exercise1_1/world.gd
@@ -3,9 +3,6 @@ extends Node2D
 func _process(_delta: float) -> void:
 	if Input.is_action_pressed("restart"):
 		get_tree().reload_current_scene()
-	
-	if Input.is_action_pressed("exit"):
-		get_tree().quit()
 		
 	$LaunchAngle.text = "Angle: %.d" % rad_to_deg(-1*$Ball.launch_angle)
 	$Power.text = "Power: %.d" % $Ball.power
